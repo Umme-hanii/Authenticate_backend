a426a096f3f458ebb923284615381f9034691c33
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-03 22:19:57 +0530

    Create User and Role models.

---
+++ models/User.js
@@ -0,0 +1,49 @@
+import { mongoose, Schema } from 'mongoose'
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      required: false,
+      default:
+        'https://www.seekpng.com/png/detail/966-9665493_my-profile-icon-blank-profile-image-circle.png',
+    },
+    isAdmin: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    roles: {
+      type: [Schema.Types.ObjectId],
+      required: true,
+      ref: 'Role',
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('User', UserSchema)
