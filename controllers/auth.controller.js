1abbf45c29db0721d5010b51fce9727148e01e3d
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-04 16:46:46 +0530

    Implement user login
    - using bcryptjs to store hashed passwords during registration.
    - also using bcryptjs to compare password entered while login with stored hashedPassword.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,3 +24,21 @@ export const registerUser = async (req, res) => {
     return res.status(400).send('Bad Request')
   }
 }
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      return res.status(404).send(`Email or Password is incorrect`)
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res.status(404).send(`Email or Password is incorrect`)
+      }
+      return res.status(500).json({ user: user, msg: 'You are logged in!' })
+    }
+  } catch (error) {
+    return res.status(400).send('Bad Request')
+  }
+}
