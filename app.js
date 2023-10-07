afa5a6116d5fabd214ee2514a3b258d415a32740
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-08 01:38:01 +0530

    Install cors package to tackle CORS erros.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import { connectDB } from './db/connect.js'
 import roleRoute from './routes/role.js'
@@ -15,6 +16,12 @@ const port = process.env.PORT | 5000
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  })
+)
 app.use('/api/role', roleRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
